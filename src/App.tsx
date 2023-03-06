e47611ec39b0746b5e767a0c1929ca3f7a38868d
marcosmbm <marmbmti@gmail.com>
2023-03-06 20:24:14 -0300

    page signIn and register

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import RoutesApp from "./routes";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
 export default function App() {
   return (
-   <>
+   <ChakraProvider>
      <RoutesApp/>
-   </>
+   </ChakraProvider>
   );
 }
--- src/pages/auth/SignUp/index.tsx
+++ src/pages/auth/SignUp/index.tsx
@@ -1,9 +1,116 @@
 import React from 'react'; 
 
+import {AtSignIcon, EmailIcon, LockIcon} from '@chakra-ui/icons';
+import {Flex, Stack, Image, Heading, Box, FormControl, InputGroup, InputLeftElement, Input, Button} from '@chakra-ui/react';
+
+import logo from '../../../assets/logo.png';
+
 export default function SignUp() {
   return (
-   <div>
-     <h1>SignUp</h1>
-   </div>
+   <Flex
+      flexDirection={'column'}
+      width={'100vw'}
+      height={'100vh'}
+      backgroundColor={"gray.200"}
+      alignItems={"center"}
+      justifyContent={"center"}
+   >
+     <Stack
+        flexDir={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        mb="2"
+     >
+        <Image
+          src={logo}
+          alt="Logo da aplicação"
+          boxSize={"80px"}
+          objectFit={"contain"}
+        />
+
+        <Heading>
+            Cadastre-se
+        </Heading>
+
+        <Box
+          minW={{base: '90%', md: '470'}}
+        >
+          <form>
+            <Stack
+              spacing={4}
+              p="1rem"
+              backgroundColor={"whiteAlpha.900"}
+              boxShadow={"md"}
+              borderRadius="md"
+            >
+                <FormControl>
+                    <InputGroup>
+                        <InputLeftElement
+                          pointerEvents={"none"}
+                          children={<AtSignIcon color={"gray.300"}/>}
+                        />
+
+                        <Input
+                          type={"text"}
+                          placeholder="Seu Nome"
+                        />
+                    </InputGroup>
+                </FormControl>
+
+                <FormControl>
+                    <InputGroup>
+                        <InputLeftElement
+                          pointerEvents={"none"}
+                          children={<EmailIcon color={"gray.300"}/>}
+                        />
+
+                        <Input
+                          type={"email"}
+                          placeholder="Endereço de email"
+                        />
+                    </InputGroup>
+                </FormControl>
+
+                <FormControl>
+                    <InputGroup>
+                        <InputLeftElement
+                          pointerEvents={"none"}
+                          children={<LockIcon color={"gray.300"}/>}
+                        />
+
+                        <Input
+                          type={"password"}
+                          placeholder="Senha"
+                        />
+                    </InputGroup>
+                </FormControl>
+
+                <FormControl>
+                    <InputGroup>
+                        <InputLeftElement
+                          pointerEvents={"none"}
+                          children={<LockIcon color={"gray.300"}/>}
+                        />
+
+                        <Input
+                          type={"password"}
+                          placeholder="Confirmar Senha"
+                        />
+                    </InputGroup>
+                </FormControl>
+
+
+                <Button type={"submit"} variant="solid" colorScheme="green">
+                    Cadastrar
+                </Button>
+
+                <Button type={"button"} variant="link" colorScheme="green">
+                    Voltar
+                </Button>
+            </Stack>
+          </form>
+        </Box>
+     </Stack>
+   </Flex>
   );
 }
