afc00eca2a4b821068bcb982d4ff340fa7bafdc4
marcosmbm <marmbmti@gmail.com>
2023-03-11 23:40:02 -0300

    login

---
+++ src/components/Alerts/index.tsx
@@ -0,0 +1,49 @@
+import { Alert, AlertDescription, AlertIcon, AlertTitle, CloseButton } from '@chakra-ui/react';
+import { AlertStatus } from '@chakra-ui/react';
+
+export interface AlertsProps{
+    message: string;
+    status?: AlertStatus;
+    closeAlert?: () => void;
+}
+
+interface titlesStatus{
+    error: string;
+    success: string;
+    warning: string;
+    info: string;
+    loading: string;
+}
+
+export default function Alerts({message, status, closeAlert = () => {}}: AlertsProps) {
+
+    function getTitle(status: string = 'Erro'): string{
+        let obj: titlesStatus = {
+            error: 'Erro',
+            success: 'Sucesso',
+            warning: 'Atenção',
+            info: 'Informação',
+            loading: 'Carregando'
+        }
+
+        return obj[status as keyof titlesStatus];
+    }
+
+    const title = getTitle(status);
+
+  return (
+    <Alert status={status} mt={2}>
+        <AlertIcon />
+        <AlertTitle>{title}: </AlertTitle>
+        <AlertDescription>{message}</AlertDescription>
+        
+        <CloseButton    
+            size='sm'     
+            position='absolute'
+            right={0}
+            top={0}
+            onClick={closeAlert}
+        />
+    </Alert>
+  );
+}
--- src/pages/auth/SignIn/index.tsx
+++ src/pages/auth/SignIn/index.tsx
@@ -1,43 +1,72 @@
-import React from 'react'; 
-import {useNavigate} from 'react-router-dom';
-import {useForm} from 'react-hook-form';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
-import {EmailIcon, LockIcon} from '@chakra-ui/icons';
-import {Flex, Stack, Image, Heading, Box, FormControl, InputGroup, InputLeftElement, Input, FormHelperText, Link, Button} from '@chakra-ui/react';
+import { EmailIcon, LockIcon } from '@chakra-ui/icons';
+import { Flex, Stack, Image, Heading, Box, FormControl, InputGroup, InputLeftElement, Input, FormHelperText, Link, Button } from '@chakra-ui/react';
 
 import logo from '../../../assets/logo.png';
 import RouterLink from '../../../components/RouterLink';
+import Alerts from '../../../components/Alerts';
+
+import { AlertStatus } from '@chakra-ui/react';
 
 
 export default function SignIn() {
 
   const navigate = useNavigate();
-  const {register, handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm();
 
-  function handleLogin(){
-    // verificar se os dados de email e senha foram digitados
-    // requisição para o backend
-    // em caso de sucesso, ir para a rota home
-    
-    localStorage.setItem('@cadastro_clientes', 'teste')
-    navigate('/dashboard', {replace: true});
+  const [message, setMessage] = useState('');
+  const [status,setStatus] = useState<AlertStatus>('error');
+
+  async function handleLogin(data: any) {
+    setMessage('');
+
+    if(data.email.trim() === '' || data.password.trim() === ''){
+      setStatus('info');
+      setMessage('Preencha todos os campos!');
+      return
+    }
+
+    await fetch('http://localhost:3001/v1/sign-in', {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+      .then(async (response) => {
+        const json = await response.json();
+
+        if (response.ok) {
+          localStorage.setItem('@cadastro_clientes', json.token);
+          navigate('/dashboard', { replace: true });
+        }
+        else {
+          setMessage(json.message)
+        }
+      })
+      .catch((error) => {
+        setMessage(error);
+      })
   }
 
   return (
-   <Flex
+    <Flex
       flexDirection={'column'}
       width={'100vw'}
       height={'100vh'}
       backgroundColor={"gray.200"}
       alignItems={"center"}
       justifyContent={"center"}
-   >
-     <Stack
+    >
+      <Stack
         flexDir={"column"}
         alignItems={"center"}
         justifyContent={"center"}
         mb="2"
-     >
+      >
         <Image
           src={logo}
           alt="Logo da aplicação"
@@ -46,11 +75,11 @@ export default function SignIn() {
         />
 
         <Heading>
-            Cadastro de Clientes
+          Cadastro de Clientes
         </Heading>
 
         <Box
-          minW={{base: '90%', md: '470'}}
+          minW={{ base: '90%', md: '470' }}
         >
           <form onSubmit={handleSubmit(handleLogin)}>
             <Stack
@@ -60,51 +89,61 @@ export default function SignIn() {
               boxShadow={"md"}
               borderRadius="md"
             >
-                <FormControl>
-                    <InputGroup>
-                        <InputLeftElement
-                          pointerEvents={"none"}
-                          children={<EmailIcon color={"gray.300"}/>}
-                        />
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents={"none"}
+                    children={<EmailIcon color={"gray.300"} />}
+                  />
 
-                        <Input
-                          type={"email"}
-                          placeholder="Endereço de email"
-                          {...register('email')}
-                        />
-                    </InputGroup>
-                </FormControl>
+                  <Input
+                    type={"email"}
+                    placeholder="Endereço de email"
+                    {...register('email')}
+                  />
+                </InputGroup>
+              </FormControl>
 
-                <FormControl>
-                    <InputGroup>
-                        <InputLeftElement
-                          pointerEvents={"none"}
-                          children={<LockIcon color={"gray.300"}/>}
-                        />
+              <FormControl>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents={"none"}
+                    children={<LockIcon color={"gray.300"} />}
+                  />
 
-                        <Input
-                          type={"password"}
-                          placeholder="********"
-                          {...register('password')}
-                        />
-                    </InputGroup>
+                  <Input
+                    type={"password"}
+                    placeholder="********"
+                    {...register('password')}
+                  />
+                </InputGroup>
 
-                    <FormHelperText textAlign={"right"}>
-                      <Link>Esqueceu a senha?</Link>
-                    </FormHelperText>
-                </FormControl>
+                <FormHelperText textAlign={"right"}>
+                  <Link>Esqueceu a senha?</Link>
+                </FormHelperText>
+              </FormControl>
 
-                <Button type={"submit"} variant="solid" colorScheme="green">
-                    Entrar
-                </Button>
+              <Button type={"submit"} variant="solid" colorScheme="green">
+                Entrar
+              </Button>
             </Stack>
+
+            {
+              message.length > 0 &&
+                <Alerts
+                  status={status}
+                  message={message}
+                  closeAlert={() => setMessage('')}
+                />
+            }
+
           </form>
         </Box>
-     </Stack>
+      </Stack>
 
-     <Box>
+      <Box>
         Ainda não possui cadastro? <RouterLink to='/register'>Cadastre-se</RouterLink>
-     </Box>
-   </Flex>
+      </Box>
+    </Flex>
   );
 }
